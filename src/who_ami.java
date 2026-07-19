a32748ecd78c02b3a0a82d5e50a8ad6c99f2540c
rhinsova <rintarotyyy@gmail.com>
2026-07-19 17:48:54 +0800

    just a test switch

---
+++ src/who_ami.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class who_ami {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        String playerAnswer;
+
+        System.out.println("Do i know you from somewhere?");
+
+        System.out.print("Yes/No: ");
+        playerAnswer = scanner.nextLine().toLowerCase();
+
+        switch (playerAnswer){
+
+            case "yes" -> System.out.println("+ Social Skills");
+            case "no" -> System.out.println("- Team Chemistry");
+
+        }
+    }
+
+
+
+}
