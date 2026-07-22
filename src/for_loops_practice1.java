d5f325bb211bb1e967b8dcb955c9e02ea404d9c1
rhinsova <rintarotyyy@gmail.com>
2026-07-22 22:24:07 +0800

    Multiplication Table Program

---
+++ src/for_loops_practice1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class for_loops_practice1 {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        // 07/21/26
+        // a little bit rusty, got too focused on math
+
+        // VARIABLE
+        int userNumber;
+
+        // SHORT WELCOME MESSAGE
+        System.out.println("== MULTIPLICATION TABLE == ");
+
+        //ASK FOR USER INPUT
+        System.out.print("Enter a number: ");
+        userNumber = scanner.nextInt();
+
+        // FOR LOOP WON'T END TILL IT HITS 10
+        for (int i = 1; i <= 10 ; i++){
+            System.out.printf("%d x %d = %d\n",userNumber, i, userNumber * i);
+
+        }
+    }
+}
